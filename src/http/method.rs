e64116c2758f5929fee03a8cb4abea5a0b65e44e
hesham1221 <hishammohmed12345@gmail.com>
2023-01-08 02:38:21 +0200

    feat : add basic modules and structs for the http server

---
+++ src/http/method.rs
@@ -0,0 +1,11 @@
+pub enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    PATHCH,
+    TRACE,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// use http::Request;
+// use http::Method;
+use server::Server;
+mod server;
+mod http;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
